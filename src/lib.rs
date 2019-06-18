521a5e2b3924de63a03525673d233c31ae2aca78
Amy Jie <git.xvrqt.com>
2019-06-18 16:49:48 -0700

    Added in a library test suite.

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,114 @@ mod tests {
 	use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn constructor() {
+    	let mut add_two = Memoizer::new(|n| {
+    		n + 2
+    	});
+        assert_eq!(4, add_two.value(2));
+    }
+
+    #[test]
+    fn memoization() {
+    	let mut add_two = Memoizer::new(|n| {
+    		n + 2
+    	});
+        assert_eq!(4, add_two.value(2));
+        assert_eq!(4, add_two.value(2));
+    }
+
+    #[test]
+    fn multiple() {
+    	let mut add_two = Memoizer::new(|n| {
+    		n + 2
+    	});
+        assert_eq!(4, add_two.value(2));
+        assert_eq!(5, add_two.value(3));
+    }
+
+    #[test]
+    fn mixed_types() {
+    	let mut length = Memoizer::new(|s: &str| {
+    		s.len()
+    	});
+        assert_eq!("gaygirls".len(), length.value("gaygirls"));
+        assert_eq!("gaygirls".len(), length.value("gaygirls"));
+
+        assert_eq!(3, length.value("gay"));
+    }
+
+    #[derive(Debug, Clone, Hash)]
+    struct Dummy {
+    	pub field: usize,
+    	pub field2: String
+    }
+
+    impl PartialEq for Dummy {
+    	fn eq(&self, other: &Dummy) -> bool {
+    		self.field == other.field && self.field2 == other.field2
+    	}
+    }
+
+    impl Eq for Dummy {}
+
+    #[test]
+    fn structs() {
+    	let d = Dummy { field: 1, field2: String::from("gay") };
+    	let mut calc = Memoizer::new(|d: Dummy| {
+    		d.field + d.field2.len()
+    	});
+
+        assert_eq!(4, calc.value(d));
+    }
+
+    #[test]
+    fn structs_by_ref() {
+    	let d = Dummy { field: 1, field2: String::from("gay") };
+    	let mut calc = Memoizer::new(|d: &Dummy| {
+    		let field = d.field + d.field2.len();
+    		let field2 = d.field2.clone();
+    		let new_dummy = Dummy { field, field2 };
+    		new_dummy
+    	});
+
+    	let mut new = calc.value(&d);
+        assert_eq!(Dummy { field: 4, field2: String::from("gay") }, new);
+        new.field = 0;
+        assert_eq!(Dummy { field: 4, field2: String::from("gay") }, calc.value(&d));
+    }
+
+    #[test]
+    fn heap_allocated() {
+    	let v = vec![1, 2, 3];
+    	let v2 = vec![1, 2, 3];
+    	let mut calc = Memoizer::new(|v: Vec<u32>| {
+    		v.len()
+    	});
+
+    	assert_eq!(3, calc.value(v));
+    	assert_eq!(3, calc.value(v2));
+    }
+
+    #[test]
+    fn heap_returned() {
+    	let v = vec![1, 2, 3];
+    	let v2 = vec![1, 2, 3];
+    	let mut calc = Memoizer::new(|v: Vec<u32>| {
+    		let mut r = vec![3,2,1];
+    		r.extend(v);
+    		r
+    	});
+
+    	let mut calculated_v = calc.value(v);
+    	let assert_v = vec![3,2,1,1,2,3];
+    	for (i, e ) in calculated_v.iter().enumerate() {
+    		assert_eq!(calculated_v[i], assert_v[i]);
+    	}
+    	calculated_v[0] = 23;
+
+    	let calculated_v = calc.value(v2);
+    	for (i, e ) in calculated_v.iter().enumerate() {
+    		assert_eq!(calculated_v[i], assert_v[i]);
+    	}
     }
 }
